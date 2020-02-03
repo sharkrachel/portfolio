72fe5c1bf69d1d088aa270f72a2da0ffeb67b977
Rachel Sipes <rasipes@gmail.com>
2020-02-02 19:42:46 -0500

    changed icon on menu

--- assets/script/logic.js
+++ assets/script/logic.js
@@ -1,20 +1,17 @@
 
 
-//Navbar 
+// Materialize navbar 
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {edge: 'right'});
   });
 
-  
-
-  // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
-  // var collapsibleElem = document.querySelector('.collapsible');
-  // var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
-
-  // Or with jQuery
-
   $(document).ready(function(){
     // $('.sidenav').sidenav();
     $('.sidenav').sidenav({ edge: 'right' });
+
+
+
+
+    
   });
